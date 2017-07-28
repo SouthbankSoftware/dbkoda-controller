3550782d866867c1de1f43b2a5fe2ef5c4b9ff26
Guan Gui <root@guiguan.net>
2017-07-28 14:23:25 +1000

    Got rid of uglify-js (horrible :(). Used a simple script to strip away comments

--- bin/combineJavascripts.js
+++ bin/combineJavascripts.js
@@ -5,6 +5,9 @@
 const shelljs = require('shelljs');
 const os = require('os');
 const rimraf = require('rimraf');
+const strip = require('strip-comments');
+const path = require('path');
+const fs = require('fs');
 
 rimraf('lib/all-in-one.js', [], () => {
   let command;
@@ -14,4 +17,9 @@ rimraf('lib/all-in-one.js', [], () => {
     command = 'cat src/mongoScripts/*.js >> lib/all-in-one.js';
   }
   shelljs.exec(command);
+
+  const scriptPath = path.resolve(__dirname, '../lib/all-in-one.js');
+  const script = fs.readFileSync(scriptPath, { encoding: 'utf8' });
+  const strippedScript = strip(script);
+  fs.writeFileSync(scriptPath, strippedScript);
 });
