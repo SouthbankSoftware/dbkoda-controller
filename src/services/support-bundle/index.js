6b75453109e6173d69b7bd03fd9fbe992518c354
michaeljharrison <michael.j.harrison@outlook.com>
2017-09-25 12:19:16 +1000

    Added archiver library and archiver for support bundles.

--- src/services/support-bundle/index.js
+++ src/services/support-bundle/index.js
@@ -20,6 +20,8 @@
 /* eslint class-methods-use-this: 0 */
 import path from 'path';
 import os from 'os';
+import archiver from 'archiver';
+import fs from 'fs';
 
 const hooks = require('./hooks');
 
@@ -29,7 +31,7 @@ class SupportBundleService {
     this.docs = {
       description: 'A service for creating support bundles.',
       get: {
-        description: 'create a support bundle.',
+        description: 'create a support bundle and return location.',
         parameters: [],
       },
     };
@@ -37,38 +39,124 @@ class SupportBundleService {
 
   async get() {
     const bundlePath = await this._createBundle();
+    l.info('Returning support bundle location to UI.');
     return Promise.resolve(bundlePath);
   }
 
-  _createBundle() {
-    return new Promise((resolve) => {
-      // Do all the logic to create a bundle.
-      let logPath = path.resolve('controller-dev.log');
-      let configPath = path.resolve(os.homedir(), '.dbKoda', 'config.yml');
-      let statePath = path.resolve(os.homedir(), '.dbKoda', 'stateStore.json');
+  _bundleFiles(bundlePath, filePaths) {
+    return new Promise((resolve, reject) => {
+      l.info('Creating new bundle at:', bundlePath);
 
-      if (global.IS_PROD) {
-        l.info('Creating new support bundle.');
-        l.info('The following paths will be added to a support bundle: ');
-        l.info(logPath);
-        l.info(configPath);
-        l.info(statePath);
-        logPath = process.env.LOG_PATH;
-        configPath = process.env.CONFIG_PATH;
-        statePath = path.resolve(os.homedir(), '.dbKoda', 'config.yml');
-        // Create Support Bundle at Log Location.
+      const output = fs.createWriteStream(bundlePath);
+      const archive = archiver('zip', {
+        zlib: { level: 7 }, // Sets compression level
+      });
 
-        // Return location of file.
-        resolve(logPath);
-      } else {
-        l.info('Creating new support bundle (dev mode).');
-        l.info('The following paths will be added to a support bundle: ');
-        l.info(logPath);
-        l.info(configPath);
-        l.info(statePath);
+      // listen for all archive data to be written.
+      output.on('close', () => {
+        l.info(archive.pointer() + ' total byes');
+        l.info(
+          'archiver has been finalized and the output file descriptor has closed.',
+        );
+        resolve(bundlePath);
+      });
+
+      // good practice to catch warnings
+      archive.on('warning', (err) => {
+        if (err.code === 'ENOENT') {
+          l.warn(err);
+        } else {
+          reject(err);
+        }
+      });
+
+      archive.on('error', (err) => {
+        reject(err);
+      });
+
+      archive.pipe(output);
+
+      // Append controller log.
+      if (fs.existsSync(filePaths.controllerLogPath)) {
+        archive.append(fs.createReadStream(filePaths.controllerLogPath), {
+          name: 'controllerLog.json',
+        });
       }
-      const filePath = 'abc';
-      resolve(filePath);
+
+      // Append dbKoda log.
+      if (filePaths.dbKodaLogPath && fs.existsSync(filePaths.dbKodaLogPath)) {
+        archive.append(fs.createReadStream(filePaths.dbKodaLog), {
+          name: 'dbKodaLog.json',
+        });
+      }
+
+      // Append stateStore.
+      if (fs.existsSync(filePaths.statePath)) {
+        archive.append(fs.createReadStream(filePaths.statePath), {
+          name: 'stateStore.json',
+        });
+      }
+
+      // Append config.yml
+      if (fs.existsSync(filePaths.configPath)) {
+        archive.append(fs.createReadStream(filePaths.configPath), {
+          name: 'config.yml',
+        });
+      }
+
+      // Finalize the archive
+      archive.finalize();
+    });
+  }
+
+  _createBundle() {
+    return new Promise((resolve, reject) => {
+      // Do all the logic to create a bundle.
+      let controllerLogPath = path.resolve('controller-dev.log');
+      let dbKodaLogPath;
+      let configPath = path.resolve(os.homedir(), '.dbKoda', 'config.yml');
+      const statePath = path.resolve(
+        os.homedir(),
+        '.dbKoda',
+        'stateStore.json',
+      );
+      const bundlePath = path.resolve(
+        os.homedir(),
+        '.dbKoda',
+        'supportBundle.zip',
+      );
+
+      l.info('Creating new support bundle (dev mode).');
+      l.info('The following paths will be added to a support bundle: ');
+      if (global.IS_PROD) {
+        controllerLogPath = process.env.LOG_PATH;
+        configPath = process.env.CONFIG_PATH;
+        dbKodaLogPath = path.dirname(controllerLogPath) + '';
+      }
+      l.info('App Logs: ', dbKodaLogPath);
+      l.info('Controller Logs: ', controllerLogPath);
+      l.info('Config File: ', configPath);
+      l.info('State Store: ', statePath);
+
+      // Create filePaths object
+      const filePaths = {
+        controllerLogPath,
+        dbKodaLogPath,
+        statePath,
+        configPath,
+      };
+
+      // Create Support Bundle at Log Location.
+      this._bundleFiles(bundlePath, filePaths)
+        .then((res) => {
+          // Return location of file.
+          l.info('Support bundles at: ', res);
+          resolve(res);
+        })
+        .catch((err) => {
+          l.error('Support bundle failed to create with err: ', err);
+          reject(err);
+        });
     });
   }
 }
