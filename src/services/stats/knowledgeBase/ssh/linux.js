c635ec18b9dd8829d47d4edb339d9aef96dd2b40
Yi Zhao <zhaoyi0113@gmail.com>
2017-12-22 08:53:44 +1100

    add validate on usage value

--- src/services/stats/knowledgeBase/ssh/linux.js
+++ src/services/stats/knowledgeBase/ssh/linux.js
@@ -79,7 +79,10 @@ const common = {
           };
           data.cpu = data.details.cpu.us + data.details.cpu.sy + data.details.cpu.wa + data.details.cpu.st;
           const totalMemory = data.details.memory.swpd + data.details.memory.buff + data.details.memory.cache + data.details.memory.free;
-          const usedMemory = data.details.memory.swpd + data.details.memory.buff;
+          let usedMemory = data.details.memory.swpd + data.details.memory.buff;
+          if (usedMemory > totalMemory) {
+            usedMemory = totalMemory;
+          }
           data.memory = parseFloat((usedMemory / totalMemory) * 100, 10);
           output.value = data;
         }
