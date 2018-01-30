b6301bf552d8537a7f3c21900e8ece9bcd9a38c3
Yi Zhao <zhaoyi0113@gmail.com>
2018-01-30 12:35:14 +1100

    update memory parser

--- src/services/stats/knowledgeBase/ssh/linux.js
+++ src/services/stats/knowledgeBase/ssh/linux.js
@@ -26,6 +26,7 @@ import _ from 'lodash';
 
 const commandParsers = {
   'cpuMemory': (d) => {
+    log.debug('cpu memory output:', d);
     // parse the vmstat command output
     const splited = d.split(os.platform() === 'win32' ? '\n\r' : '\n');
     if (!splited || splited.length < 4) {
@@ -33,61 +34,55 @@ const commandParsers = {
     }
     const output: any = {timestamp: (new Date()).getTime()};
     const line = splited[3];
-      if (line.match(/procs/) && line.match(/memory/)) {
-        // this is header, ignore
-      } else if (line.match(/swpd/ && line.match(/buff/))) {
-        // this is header
-      } else {
-        const items = _.without(line.split(' '), '');
-        if (items.length >= 17) {
-          const intItems = items.map(item => parseInt(item, 10));
-          const data: any = {
-            details: {
-              procs: {
-                r: intItems[0], // The number of processes waiting for run time
-                b: intItems[1], // The number of processes in uninterruptible sleep,
-              },
-              memory: {
-                swpd: intItems[2], // the amount of virtual memory used.
-                free: intItems[3], // the amount of idle memory
-                buff: intItems[4], // the amount of memory used as buffers
-                cache: intItems[5], // the amount of memory used as cache
-              },
-              swap: {
-                si: intItems[6], // Amount of memory swapped in from disk
-                so: intItems[7], // Amount of memory swapped to disk (/s).
-              },
-              io: {
-                bi: intItems[8], // Blocks received from a block device (blocks/s).
-                bo: intItems[9], // Blocks sent to a block device (blocks/s).
-              },
-              system: {
-                in: intItems[10], // The number of interrupts per second, including the clock.
-                cs: intItems[11], // The number of context switches per second
-              },
-              cpu: {
-                us: intItems[12], //  Time spent running non-kernel code. (user time, including nice time)
-                sy: intItems[13], //  Time spent running kernel code. (system time)
-                id: intItems[14], //  Time spent idle. Prior to Linux 2.5.41, this includes IO-wait time
-                wa: intItems[15], //  Time spent waiting for IO. Prior to Linux 2.5.41, included in idle.
-                st: intItems[16], //  Time stolen from a virtual machine. Prior to Linux 2.6.11, unknown
-              }
-            }
-          };
-          data.cpu = data.details.cpu.us + data.details.cpu.sy + data.details.cpu.wa + data.details.cpu.st;
-          if (data.cpu > 100) {
-            data.cpu = 100;
+    const items = _.without(line.split(' '), '');
+    if (items.length >= 17) {
+      const intItems = items.map(item => parseInt(item, 10));
+      const data: any = {
+        details: {
+          procs: {
+            r: intItems[0], // The number of processes waiting for run time
+            b: intItems[1], // The number of processes in uninterruptible sleep,
+          },
+          memory: {
+            swpd: intItems[2], // the amount of virtual memory used.
+            free: intItems[3], // the amount of idle memory
+            buff: intItems[4], // the amount of memory used as buffers
+            cache: intItems[5], // the amount of memory used as cache
+          },
+          swap: {
+            si: intItems[6], // Amount of memory swapped in from disk
+            so: intItems[7], // Amount of memory swapped to disk (/s).
+          },
+          io: {
+            bi: intItems[8], // Blocks received from a block device (blocks/s).
+            bo: intItems[9], // Blocks sent to a block device (blocks/s).
+          },
+          system: {
+            in: intItems[10], // The number of interrupts per second, including the clock.
+            cs: intItems[11], // The number of context switches per second
+          },
+          cpu: {
+            us: intItems[12], //  Time spent running non-kernel code. (user time, including nice time)
+            sy: intItems[13], //  Time spent running kernel code. (system time)
+            id: intItems[14], //  Time spent idle. Prior to Linux 2.5.41, this includes IO-wait time
+            wa: intItems[15], //  Time spent waiting for IO. Prior to Linux 2.5.41, included in idle.
+            st: intItems[16], //  Time stolen from a virtual machine. Prior to Linux 2.6.11, unknown
           }
-          const totalMemory = data.details.memory.swpd + data.details.memory.buff + data.details.memory.cache + data.details.memory.free;
-          let usedMemory = data.details.memory.swpd + data.details.memory.buff + data.details.memory.cache;
-          if (usedMemory > totalMemory) {
-            usedMemory = totalMemory;
-          }
-          data.memory = parseFloat((usedMemory / totalMemory) * 100, 10);
-          data.io = data.details.io;
-          output.value = data;
         }
+      };
+      data.cpu = data.details.cpu.us + data.details.cpu.sy + data.details.cpu.wa + data.details.cpu.st;
+      if (data.cpu > 100) {
+        data.cpu = 100;
       }
+      const totalMemory = data.details.memory.swpd + data.details.memory.buff + data.details.memory.cache + data.details.memory.free;
+      let usedMemory = data.details.memory.swpd + data.details.memory.buff + data.details.memory.cache;
+      if (usedMemory > totalMemory) {
+        usedMemory = totalMemory;
+      }
+      data.memory = parseFloat((usedMemory / totalMemory) * 100, 10);
+      data.io = data.details.io;
+      output.value = data;
+    }
     return output;
   },
   'disk': (output) => {
