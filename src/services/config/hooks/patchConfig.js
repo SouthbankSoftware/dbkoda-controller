20b563a597af22427e91ea5a5f2cc0ef2c1f31ae
Yi Zhao <zhaoyi0113@gmail.com>
2018-04-27 10:55:54 +1000

    fix eslint error

--- src/services/config/hooks/patchConfig.js
+++ src/services/config/hooks/patchConfig.js
@@ -36,11 +36,16 @@ import _ from 'lodash';
 import diff from 'deep-diff';
 import os from 'os';
 import path from 'path';
-import { execSync } from 'child_process';
-import { configDefaults } from '../configSchema';
-import { isDockerCommand } from '../../../controllers/docker';
+import {execSync} from 'child_process';
+import {configDefaults} from '../configSchema';
+import {isDockerCommand} from '../../../controllers/docker';
 
-const SIBLING_MONGO_CMD = ['mongodumpCmd', 'mongorestoreCmd', 'mongoimportCmd', 'mongoexportCmd'];
+const SIBLING_MONGO_CMD = [
+  'mongodumpCmd',
+  'mongorestoreCmd',
+  'mongoimportCmd',
+  'mongoexportCmd',
+];
 
 const getMongoCmd = () => {
   let mongoCmd = null;
@@ -49,7 +54,9 @@ const getMongoCmd = () => {
     if (os.platform() === 'win32') {
       mongoCmd = 'mongo.exe';
     } else {
-      mongoCmd = execSync("bash -lc 'which mongo'", { encoding: 'utf8' }).trim();
+      mongoCmd = execSync("bash -lc 'which mongo'", {
+        encoding: 'utf8',
+      }).trim();
       const tmp = mongoCmd.split('\n');
       if (tmp.length > 0) {
         mongoCmd = tmp[tmp.length - 1];
@@ -79,7 +86,8 @@ const updateMongoCmd = (mongoCmd, nextConfig) => {
   }
 };
 
-export const getDumpableConfigView = (config: *) => _.pick(config, _.keys(configDefaults));
+export const getDumpableConfigView = (config: *) =>
+  _.pick(config, _.keys(configDefaults));
 
 const HISTORY_SIZE_PATH = 'performancePanel.historySize';
 const HISTORY_BRUSH_SIZE_PATH = 'performancePanel.historyBrushSize';
@@ -88,31 +96,43 @@ const checkHistoryConfig = (currentConfig, nextConfig) => {
   const errorsObj = {};
 
   const currentHistorySize = _.get(currentConfig, HISTORY_SIZE_PATH);
-  const currentHistoryBrushSize = _.get(currentConfig, HISTORY_BRUSH_SIZE_PATH);
+  const currentHistoryBrushSize = _.get(
+    currentConfig,
+    HISTORY_BRUSH_SIZE_PATH
+  );
   const nextHistorySize = _.get(nextConfig, HISTORY_SIZE_PATH);
   const nextHistoryBrushSize = _.get(nextConfig, HISTORY_BRUSH_SIZE_PATH);
   const minimumHistorySize = nextHistoryBrushSize || currentHistoryBrushSize;
   const maximumHistoryBrushSize = nextHistorySize || currentHistorySize;
 
   if (nextHistorySize && nextHistorySize < minimumHistorySize) {
-    errorsObj[`config.${HISTORY_SIZE_PATH}`] = `should be >= ${minimumHistorySize}`;
+    errorsObj[
+      `config.${HISTORY_SIZE_PATH}`
+    ] = `should be >= ${minimumHistorySize}`;
   }
 
   if (nextHistoryBrushSize && nextHistoryBrushSize > maximumHistoryBrushSize) {
-    errorsObj[`config.${HISTORY_BRUSH_SIZE_PATH}`] = `should be <= ${maximumHistoryBrushSize}`;
+    errorsObj[
+      `config.${HISTORY_BRUSH_SIZE_PATH}`
+    ] = `should be <= ${maximumHistoryBrushSize}`;
   }
 
   if (!_.isEmpty(errorsObj)) {
     throw new errors.BadRequest('Data does not match schema', {
-      errors: errorsObj
+      errors: errorsObj,
     });
   }
 };
 
 export default () =>
   processItems((context, item) => {
-    const { config: nextConfig, emitChangedEvent, forceSave, fromConfigYml } = item;
-    const { service } = context;
+    const {
+      config: nextConfig,
+      emitChangedEvent,
+      forceSave,
+      fromConfigYml,
+    } = item;
+    const {service} = context;
 
     // `ajv` should guard most of the correctness by now, but some post checkings are also necessary
     checkHistoryConfig(global.config, nextConfig);
@@ -125,15 +145,18 @@ export default () =>
     }
 
     const changed = {};
-    const differences = diff(getDumpableConfigView(global.config), nextConfig) || [];
+    const differences = diff(
+      getDumpableConfigView(global.config),
+      nextConfig
+    ) || [];
     let hasDeletion = false;
 
-    for (const { kind, path, lhs, rhs } of differences) {
+    for (const {kind, path, lhs, rhs} of differences) {
       // TODO: handle array modification
       if (kind === 'E' || kind === 'N') {
         changed[path.join('.')] = {
           old: lhs,
-          new: rhs
+          new: rhs,
         };
 
         // apply change
@@ -158,16 +181,19 @@ export default () =>
 
     let p;
 
-    if (forceSave || ((fromConfigYml && hasDeletion) || (!fromConfigYml && hasChanges))) {
+    if (
+      forceSave ||
+      ((fromConfigYml && hasDeletion) || (!fromConfigYml && hasChanges))
+    ) {
       l.debug(`Updating ${global.CONFIG_PATH}...`);
 
       // update config.yml
-      const { fileService, handleError } = service;
+      const {fileService, handleError} = service;
       p = fileService
         .create({
           _id: global.CONFIG_PATH,
           watching: true,
-          content: yaml.safeDump(getDumpableConfigView(global.config))
+          content: yaml.safeDump(getDumpableConfigView(global.config)),
         })
         .catch(handleError);
     } else {
